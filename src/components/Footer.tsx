be75cc37a04abb0ea1654e60866dc7a79c1ec33d
Another One <159118983+AnotherOne661@users.noreply.github.com>
2026-01-28 08:53:16 +0000

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 items-center gap-8">
         
         <div className="flex justify-center md:justify-start gap-6 font-play text-sm tracking-widest">
-          <a href="#" className="hover:text-cyber-blue transition-colors underline underline-offset-8">GITHUB</a>
+          <a href="https://github.com/AnotherOne661" className="hover:text-cyber-blue transition-colors underline underline-offset-8">GITHUB</a>
           <a href="#" className="hover:text-cyber-blue transition-colors underline underline-offset-8">LINKEDIN</a>
         </div>
 
